750e4ab6399132688d192f6b7c07df4805d0c210
Aso2001180 <83047624+Aso2001180@users.noreply.github.com>
2021-12-10 10:18:37 +0900

    Add files via upload

---
+++ EC/taikai/taikai.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>User-Out</title>
+    <link rel="icon"  type="image/x-icon" sizes="144*144" href="favicon.ico">
+    <link rel="stylesheet" href="./css/style.css"><!DOCTYPE html>
+    <head>
+        <meta charset="UTF-8">
+        <title>User-Out</title>
+        <link rel="icon"  type="image/x-icon" sizes="144*144" href="favicon.ico">
+        <link rel="stylesheet" href="./taikai/css/style.css">
+        <style>
+            body{background-color:gray;}
+            button{
+                width: 80px;
+                height:40px;
+            }
+        </style>
+    </head>
+    <body>
+    <button  class="button" onclick=history.back()>戻る</button>
+    <div class="logo">
+        <img src="logo1.png" title="九州旅行記" width="400" height="200" >
+    </div>
+    <div class = "nav">
+        <div>
+            <div class = "bgDiv">
+                <div class = "taikai">
+                    <div class ="mokujii">
+                        <h1>退会しますか？</h1>
+                    </div>
+                    <button class = "btn1">取消</button>
+                    <button class = "btn2">確認</button>
+                </div>
+        </div>
+    </div>
+    </div>
+    </body>
+</html>
